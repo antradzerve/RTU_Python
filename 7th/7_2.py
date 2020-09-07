2503dc0e4f96dc151fa955dade429f4708724516
Antra DzS <dzerveaa@gmail.com>
2020-09-07 20:41:49 +0300

    Mistake fixed

--- 7th/7_2.py
+++ 7th/7_2.py
@@ -1,13 +1,10 @@
 sentence = input("Enter text to check if palindrome:  ")
 
-new_sentence = ''.join(sentence.split()).lower()
-reverse_NS = new_sentence[::-1]
+def palindrome_check(sentence):
+    one = ''.join(sentence.split()).lower()
+    two = one[::-1]
+    return one == two
 
-def palindrome_check(one, two):
-    if one == two:
-        return True
-    return False
-
-print(palindrome_check(new_sentence, reverse_NS))
+print(palindrome_check(sentence))
 
 
